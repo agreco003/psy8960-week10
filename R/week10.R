b76ade1fd8bd5af423e543f8833ac4aee9f9a620
agreco <greco031@umn.edu>
2023-03-31 16:21:17 -0500

    Removed psuedocode line at the bottom

--- R/week10.R
+++ R/week10.R
@@ -9,7 +9,3 @@ gss_tbl <- tibble(gss_data) %>%
   mutate(workhours = as.integer(HRS1)) %>% #enables logical comparison in next line, no data loss
   filter(workhours != is.na(workhours)) %>% #2867 - (1198 NA + 23 don't know / missing) = 1646 cases as in documentation, Page 123.
   select(which(colMeans(is.na(gss_data)) <= 0.25)) #selecting all columns WHICH have colmeans of missing data less or equal to 25%. #check with colMeans(is.na(gss_tbl)) shows missingness all less than .25
-  
-
-  
-  filter(workhours != NA) 
