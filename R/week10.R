857331c91f2f460ea9236db0573d4d5db6e38032
agreco <greco031@umn.edu>
2023-03-31 16:30:39 -0500

    Quick visualization, added better comments

--- R/week10.R
+++ R/week10.R
@@ -3,9 +3,12 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(caret)
 
-#Data import and cleaning
+#Data Import and Cleaning
 gss_data <- haven::read_sav("../data/GSS2016.sav") #read_sav function not found without library call, though haven is part of the tidyverse
 gss_tbl <- tibble(gss_data) %>%
   mutate(workhours = as.integer(HRS1)) %>% #enables logical comparison in next line, no data loss
   filter(workhours != is.na(workhours)) %>% #2867 - (1198 NA + 23 don't know / missing) = 1646 cases as in documentation, Page 123.
-  select(which(colMeans(is.na(gss_data)) <= 0.25)) #selecting all columns WHICH have colmeans of missing data less or equal to 25%. #check with colMeans(is.na(gss_tbl)) shows missingness all less than .25
+  select(which(colMeans(is.na(gss_data)) <= 0.25), workhours) #selecting all columns WHICH have colmeans of missing data less or equal to 25%. #check with colMeans(is.na(gss_tbl)) shows missingness all less than .25. Selected workhours manually as it isn't included in the gss_data originally. 
+
+#Visualization
+histogram(gss_tbl$workhours)
